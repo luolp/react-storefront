1a8bd80bd3be3b3f9cca21902e337ccd86125d3a
luolp <1143079030@qq.com>
2023-09-17 00:47:54 +0800

    不让自动在url末尾加/

--- apps/saleor-app-checkout/next.config.js
+++ apps/saleor-app-checkout/next.config.js
@@ -10,7 +10,7 @@ const checkoutEmbededInStorefrontPath = "/saleor-app-checkout";
 
 /** @type {import('next').NextConfig} */
 const config = withTM({
-  trailingSlash: true,
+  trailingSlash: false,
   i18n: {
     locales: ["en-US", "pl-PL", "fr-FR", "vi-VN", "ar-AE"],
     defaultLocale: "en-US",
