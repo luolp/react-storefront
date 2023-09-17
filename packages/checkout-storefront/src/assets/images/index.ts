a4b27a77f8fca796a30211baabe9c679a45506a4
luolp <1143079030@qq.com>
2023-09-17 15:16:56 +0800

    修改Logo

--- packages/checkout-storefront/src/assets/images/index.ts
+++ packages/checkout-storefront/src/assets/images/index.ts
@@ -1,4 +1,3 @@
 import SaleorLogo from "./Saleor_logo.svg";
-import EminstoreLogo from "./eminstore_logo.png";
 
-export { SaleorLogo, EminstoreLogo };
+export { SaleorLogo };
--- packages/checkout-storefront/src/sections/PageHeader/PageHeader.tsx
+++ packages/checkout-storefront/src/sections/PageHeader/PageHeader.tsx
@@ -1,11 +1,9 @@
-import { EminstoreLogo } from "@/checkout-storefront/images";
-import { getSvgSrc } from "@/checkout-storefront/lib/svgSrc";
 import { LanguageSelect } from "@/checkout-storefront/sections/PageHeader/LanguageSelect";
 
 export const PageHeader = () => {
   return (
     <div className="page-header">
-      <img src={EminstoreLogo} alt="logo" className="logo" />
+      <img src="/eminstore_logo.png" alt="logo" className="logo" />
       <LanguageSelect />
     </div>
   );
