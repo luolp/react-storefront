f08f128fd2d9b11d1da7548904204ee28efc7933
luolp <1143079030@qq.com>
2023-09-19 21:04:47 +0800

    点击订单详情页的Logo要回到首页

--- packages/checkout-storefront/src/sections/PageHeader/PageHeader.tsx
+++ packages/checkout-storefront/src/sections/PageHeader/PageHeader.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import { LanguageSelect } from "@/checkout-storefront/sections/PageHeader/LanguageSelect";
 
 export const PageHeader = () => {
   return (
     <div className="page-header">
-      <img src="/eminstore_logo.png" alt="logo" className="logo" />
+      <Link href="/">
+        <img src="/eminstore_logo.png" alt="logo" className="logo" />
+      </Link>
       <LanguageSelect />
     </div>
   );
